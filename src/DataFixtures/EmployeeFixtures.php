df4a44d4c5a57a6ccb9b7dd5085549ee3fb590fe
Vladimir Verhooste <verhooste.vladimir@gmail.com>
2020-03-26 10:41:55 +0100

    fixtures done

--- src/DataFixtures/EmployeeFixtures.php
+++ src/DataFixtures/EmployeeFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Employee;
+use App\Repository\JobRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
-class EmployeeFixtures extends Fixture
+class EmployeeFixtures extends Fixture implements DependentFixtureInterface
 {
+    public  function __construct(JobRepository $jobRepository) {
+        $this->jobRepository = $jobRepository;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        for($i=0; $i < 20; $i++) {
+
+            $employee = new Employee();
+            $employee->setFirstname( $faker->firstName );
+            $employee->setLastname( $faker->lastName );
+            $employee->setEmployementDate( $faker->dateTime($max = 'now', $timezone = null));
+            $employee->setJob( $this->jobRepository->find( rand(1, 5) ) );
+
+            $manager->persist($employee);
+        }
 
         $manager->flush();
     }
+    public function getDependencies()
+    {
+        return array(
+            JobFixtures::class
+        );
+    }
+    
 }
--- src/DataFixtures/JobFixtures.php
+++ src/DataFixtures/JobFixtures.php
@@ -2,15 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Job;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
-class JobFixtures extends Fixture
+class JobFixtures extends Fixture 
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for($i=0; $i < 5; $i++) {
+
+            $job = new Job();
+            $job->setTitle( $faker->jobTitle );
+
+            $manager->persist($job);
+        }
 
         $manager->flush();
     }
